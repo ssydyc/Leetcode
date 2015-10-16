5c5e3542308fa840e5d153a4452ef8db7d254236
Samsong <ssydyc@gmail.com>
2015-10-16 14:32:03 -0700

    Update 247_Strobogrammatic Number II .java

--- 247_Strobogrammatic Number II .java
+++ 247_Strobogrammatic Number II .java
@@ -1,1 +1,21 @@
-
+public class Solution {
+    
+    private List<String> helper(int n, int m){
+        if(n==0) return new ArrayList<String>(Arrays.asList(""));
+        if(n==1) return new ArrayList<String>(Arrays.asList("0","1","8"));
+        ArrayList<String> result=new ArrayList<String>();
+        for(String temp: helper(n-2,m)){
+            if(n!=m) result.add("0"+temp+"0");
+            result.add("1"+temp+"1");
+            result.add("8"+temp+"8");
+            result.add("6"+temp+"9");
+            result.add("9"+temp+"6");
+        }
+        return result;
+    }
+    
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n,n);
+        
+    }
+}
