d535bb989732a510f508a19a16e6000637326d0c
Samsong <ssydyc@gmail.com>
2015-10-16 15:19:02 -0700

    Update 256_Paint House.java

--- 256_Paint House.java
+++ 256_Paint House.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+    public int minCost(int[][] costs) {
+        int[] current=new int[]{0,0,0};
+        int[] next=new int[]{0,0,0};
+        for(int[] temp: costs){
+            for(int i=0;i<3;i++)
+                next[i]=Math.min(current[(i+1)%3]+temp[i],current[(i+2)%3]+temp[i]);
+            int[] tempArray=current;
+            current=next;
+            next=tempArray;
+        }
+        return Math.min(Math.min(current[0],current[1]),current[2]);
+    }
+}
