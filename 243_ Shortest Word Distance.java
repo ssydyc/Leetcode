ebe246bd5e3ac354e358e3ee06c4bc936057431d
Samsong <ssydyc@gmail.com>
2015-10-16 14:27:22 -0700

    Update 243_ Shortest Word Distance.java

--- 243_ Shortest Word Distance.java
+++ 243_ Shortest Word Distance.java
@@ -1,1 +1,34 @@
-
+public class Solution {
+    
+    private int myCompare(String word, String word1, String word2){
+        if(word.equals(word1)) return 1;
+        if(word.equals(word2)) return 2;
+        return 0;
+    }
+    
+    
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int temp=0;
+        int result=Integer.MAX_VALUE;
+        int next=0;
+        for(String word: words){
+            if(next==0){
+                int compare=myCompare(word,word1,word2);
+                if(compare==1) next=2;
+                else if(compare==2) next=1;
+            }
+            else{
+                int compare=myCompare(word, word1,word2);
+                if(compare==0) temp++;
+                else if(compare==next){
+                    result=Math.min(result,temp);
+                    if(compare==1) next=2;
+                    else next=1;
+                    temp=0;
+                }
+                else temp=0;
+            }
+        }
+        return result+1;
+    }
+}
