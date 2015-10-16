e8c47703e42aec54461a710007aa35632c8f3ebb
Samsong <ssydyc@gmail.com>
2015-10-16 16:05:53 -0700

    Update 266_ Palindrome Permutation.java

--- 266_ Palindrome Permutation.java
+++ 266_ Palindrome Permutation.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        HashMap<Character,Boolean> count= new HashMap<Character,Boolean>();
+        for(int i=0;i<s.length();i++){
+            if(!count.containsKey(s.charAt(i))) count.put(s.charAt(i),true);
+            else count.put(s.charAt(i),!count.get(s.charAt(i)));
+        }
+        int result=0;
+        for(Boolean temp: count.values()){
+            if(temp) result++;
+        }
+        if(result>1) return false;
+        else return true;
+    }
+}
