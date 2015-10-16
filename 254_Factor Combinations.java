48e2c289499a1804b1da464bd9a5c0f7010e84d7
Samsong <ssydyc@gmail.com>
2015-10-16 15:15:02 -0700

    Update 254_Factor Combinations.java

--- 254_Factor Combinations.java
+++ 254_Factor Combinations.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+    
+    private void DFS(List<List<Integer>> result, LinkedList<Integer> temp, int n, int min){
+        if(temp.size()>0){
+            temp.add(n);
+            result.add(new LinkedList(temp));
+            temp.removeLast();
+        }
+        
+        for(int i=min;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0){
+                temp.add(i);
+                DFS(result,temp,n/i,i);
+                temp.removeLast();
+            }
+        }
+    }
+    
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> result=new LinkedList<List<Integer>>();
+        LinkedList<Integer> temp= new LinkedList<Integer>();
+        DFS(result,temp,n,2);
+        return result;
+    }
+}
