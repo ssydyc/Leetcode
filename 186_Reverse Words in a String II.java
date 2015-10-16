fa84799c4f8df9567194e1521cf51256e0e6c9a0
Samsong <ssydyc@gmail.com>
2015-10-16 14:25:28 -0700

    Update 186_Reverse Words in a String II.java

--- 186_Reverse Words in a String II.java
+++ 186_Reverse Words in a String II.java
@@ -1,1 +1,26 @@
-
+public class Solution {
+    
+    private void reverseTotal(char[] s, int start, int end){
+        while(start<end){
+            char temp=s[start];
+            s[start++]=s[end];
+            s[end--]=temp;
+        }
+        return;
+    }
+    
+    public void reverseWords(char[] s) {
+        reverseTotal(s,0,s.length-1);
+        int start=0,end=0;
+        while(end<s.length){
+            if(s[end]==' ') {
+                reverseTotal(s, start, end-1);
+                start=end+1;
+            }
+            end++;
+        }
+        reverseTotal(s,start,end-1);
+        return;
+        
+    }
+}
