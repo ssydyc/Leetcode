bc2bea174ea8d192d21002245dfa8d74d9b6f9d5
Samsong <ssydyc@gmail.com>
2015-10-16 14:24:32 -0700

    Update 170_Two Sum III - Data structure design.java

--- 170_Two Sum III - Data structure design.java
+++ 170_Two Sum III - Data structure design.java
@@ -1,1 +1,30 @@
+public class TwoSum {
+    ArrayList<Integer> nums=new ArrayList<Integer>();
+    boolean sorted=false;
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	   sorted=false;
+	   nums.add(number);
+	}
 
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    if(!sorted){
+	        Collections.sort(nums);
+	        sorted=true;
+	    }
+	    int start=0, end=nums.size()-1;
+	    while(start<end){
+	        if(nums.get(start)+nums.get(end)==value) return true;
+	        else if(nums.get(start)+nums.get(end)>value) end--;
+	        else start++;
+	    }
+	    return false;
+	}
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
