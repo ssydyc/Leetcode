ce306e9db240587d345f8e30e931e8ca6cd30c42
Samsong <ssydyc@gmail.com>
2015-10-16 16:27:52 -0700

    Update 288_ Unique Word Abbreviation.java

--- 288_ Unique Word Abbreviation.java
+++ 288_ Unique Word Abbreviation.java
@@ -1,1 +1,36 @@
+public class ValidWordAbbr {
+    
+    HashMap<String,Integer> abbrev= new HashMap<String,Integer>();
+    HashSet<String> dict= new HashSet<String>();
+    
+    private String getAbbr(String temp){
+        return temp.charAt(0)+Integer.toString(temp.length()-2)+temp.charAt(temp.length()-1);
+    }
+    
+    
+    public ValidWordAbbr(String[] dictionary) {
+        for(String temp: dictionary){
+            if(temp.length()<3) continue;
+            String tempAbbrev=getAbbr(temp);
+            if(abbrev.containsKey(tempAbbrev)) abbrev.put(tempAbbrev, abbrev.get(tempAbbrev)+1);
+            else abbrev.put(tempAbbrev, 1);
+            dict.add(temp);
+        }
+    }
 
+    public boolean isUnique(String word) {
+        if(word.length()<3) return true;
+        else{
+            String temp=getAbbr(word);
+            if(dict.contains(word)&& abbrev.get(temp)>1) return false;
+            if(!dict.contains(word) && abbrev.containsKey(temp)) return false;
+            return true;
+        }
+    }
+}
+
+
+// Your ValidWordAbbr object will be instantiated and called as such:
+// ValidWordAbbr vwa = new ValidWordAbbr(dictionary);
+// vwa.isUnique("Word");
+// vwa.isUnique("anotherWord");
