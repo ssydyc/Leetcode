1d4fc8b699f6d69c8ea2d70c434d76c180ad9518
Samsong <ssydyc@gmail.com>
2015-10-16 16:29:18 -0700

    Update 293_Flip Game.java

--- 293_Flip Game.java
+++ 293_Flip Game.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> result= new ArrayList<String>();
+        StringBuilder stringS= new StringBuilder(s);
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)=='+'&& s.charAt(i-1)=='+'){
+                stringS.setCharAt(i-1, '-');
+                stringS.setCharAt(i,'-');
+                result.add(stringS.toString());
+                stringS.setCharAt(i-1, '+');
+                stringS.setCharAt(i,'+');
+            }
+        }
+        return result;
+    }
+}
