a5230002217be726f0111d559a3adc4c561cd9ee
Samsong <ssydyc@gmail.com>
2015-10-16 15:17:03 -0700

    Update 255_Verify Preorder Sequence in Binary Search Tree.java

--- 255_Verify Preorder Sequence in Binary Search Tree.java
+++ 255_Verify Preorder Sequence in Binary Search Tree.java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        int low=Integer.MIN_VALUE, i=-1;
+        for(int temp: preorder){
+            if(temp<low) return false;
+            while(i>=0&& preorder[i]<temp){
+                low=preorder[i--];
+            }
+            preorder[++i]=temp;
+        }
+        return true;
+    }
+}
