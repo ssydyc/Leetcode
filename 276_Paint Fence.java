81735621f0b5a38063232c48b067d93953a9f7f4
Samsong <ssydyc@gmail.com>
2015-10-16 16:21:22 -0700

    Update 276_Paint Fence.java

--- 276_Paint Fence.java
+++ 276_Paint Fence.java
@@ -1,1 +1,11 @@
-
+public class Solution {
+    public int numWays(int n, int k) {
+        if(n==0|| k==0) return 0;
+        int[] current= new int[3]{k,k,k};
+        int[] pre=new int[3]{k,k,k};
+        for(int i=1;i<n;i++){
+            current[0]=pre[0]*(k-1);
+            current[1]=current[0]+pre[0]
+        }
+    }
+}
