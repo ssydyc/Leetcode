7becdfdffb3b5611bf698d5715b11840c28d5cab
Samsong <ssydyc@gmail.com>
2015-10-16 16:29:48 -0700

    Update 294_Flip Game II.java

--- 294_Flip Game II.java
+++ 294_Flip Game II.java
@@ -1,1 +1,21 @@
+public class Solution {
 
+    private boolean existMoveWin(StringBuilder s, boolean flag){
+        boolean result=flag;
+        for(int i=1; i<s.length();i++){
+            if(s.charAt(i)=='+'&& s.charAt(i-1)=='+'){
+                s.setCharAt(i-1,'-');
+                s.setCharAt(i,'-');
+                result=existMoveWin(s,!flag);
+                s.setCharAt(i-1,'+');
+                s.setCharAt(i,'+');
+                if(result!=flag) break;
+            }
+        }
+        return result;
+    }
+    
+    public boolean canWin(String s) {
+        return existMoveWin(new StringBuilder(s),false);
+    }
+}
