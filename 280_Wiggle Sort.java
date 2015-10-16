a12ddc99af97703f40de78e50b2664ba4538dad5
Samsong <ssydyc@gmail.com>
2015-10-16 16:22:55 -0700

    Update 280_Wiggle Sort.java

--- 280_Wiggle Sort.java
+++ 280_Wiggle Sort.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+    
+    public void wiggleSort(int[] nums) {
+        boolean increase=true;
+        for(int i=1;i<nums.length;i++){
+            if((increase&& nums[i-1]>nums[i])|| (!increase && nums[i-1]<nums[i])){
+                int temp=nums[i-1];
+                nums[i-1]=nums[i];
+                nums[i]=temp;
+            }
+            increase=!increase;
+        }
+    }
+}
