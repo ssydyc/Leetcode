8a659e8244c42ff3894f0fd3c4185a9f52fb2d08
Samsong <ssydyc@gmail.com>
2015-10-20 15:03:34 -0700

    Update 015_3Sum.java

--- 015_3Sum.java
+++ 015_3Sum.java
@@ -1,46 +1,24 @@
 public class Solution {
-    
-    private boolean compareThree(List<Integer> array1, ArrayList<Integer> array2){
-        if(array1.get(0)!=array2.get(0)) return false;
-        if(array1.get(1)!=array2.get(1)) return false;
-        if(array1.get(2)!=array2.get(2)) return false;
-        return true;
-    }
-    
-    private boolean findArray(ArrayList<Integer> temp, ArrayList<List<Integer>> result ){
-        for(List<Integer> temp1: result){
-            if(compareThree(temp1,temp)) return true;
-        }
-        return false;
-    }
-    
-    private ArrayList<List<Integer>> findZero(int[] num){
-        ArrayList<List<Integer>> result=new ArrayList<List<Integer>>();
-        for(int i=0;i<num.length-2;i++){
-            int start=i+1;
-            int end=num.length-1;
-            while(start<end){
-                if(num[i]+num[start]+num[end]==0){
-                    ArrayList<Integer> temp=new ArrayList<Integer>();
-                    temp.add(num[i]);
-                    temp.add(num[start]);
-                    temp.add(num[end]);
-                    if(!findArray(temp,result)) result.add(temp);
-                    start++;
-                    end--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> hs= new HashSet<List<Integer>>();
+        Arrays.sort(nums);
+        List<List<Integer>> res= new ArrayList<List<Integer>>();
+        for(int i1=0; i1<nums.length-2; i1++){
+            int i2=i1+1, i3=nums.length-1;
+            while(i2!=i3){
+                int sum=nums[i1]+nums[i2]+nums[i3];
+                if(sum==0){
+                    List<Integer> temp=new ArrayList<Integer>(Arrays.asList(nums[i1],nums[i2],nums[i3]));
+                    if(!hs.contains(temp)) {
+                        res.add(temp);
+                        hs.add(temp);
+                    }
+                    i2++;
                 }
-                else if(num[i]+num[start]+num[end]>0) end--;
-                else start++;
+                else if(sum<0) i2++;
+                else i3--;
             }
         }
-        return result;
-    }
-    
-    
-    public List<List<Integer>> threeSum(int[] num) {
-        if(num.length<3) return new ArrayList<List<Integer>>();
-        Arrays.sort(num);
-        ArrayList<List<Integer>> result=findZero(num);
-        return result;
+        return res;
     }
 }
