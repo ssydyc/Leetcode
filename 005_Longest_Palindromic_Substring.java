c6ffa8b89e805b487a0fe0f1a5f2a62f08e9f1b6
Samsong <ssydyc@gmail.com>
2015-10-20 14:17:04 -0700

    Update 005_Longest_Palindromic_Substring.java

--- 005_Longest_Palindromic_Substring.java
+++ 005_Longest_Palindromic_Substring.java
@@ -1,44 +1,61 @@
-public class Solution {
-    
-    private int[] maxOdd(String s, int i){
-        // if there is odd number of palindromic Substring
-        
-        int start=i-1;
-        int end=i+1;
-        int maxLength=1;
-        while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
-            start--;
-            end++;
-            maxLength+=2;
-        }
-        int[] result={maxLength,start+1,end-1}; // First the maximum length, second the start, third end
-        return result;
-    }
-    
-    private int[] maxEven(String s, int i){
-        // if there is odd number of palindromic Substring
-        int start=i;
-        int end=i+1;
-        int maxLength=0;
-        while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
-            start--;
-            end++;
-            maxLength+=2;
-        }
-        int[] result={maxLength,start+1,end-1}; // First the maximum length, second the start, third end
-        return result;
-    }
-    
-    
-    public String longestPalindrome(String s) {
-        if(s.length()==0) return "";
-        int[] maxLength={1,0,0}; 
+public class Solution{
+    // Manacher's algorithm
+    private char[] addBoundary(String s){
+        char[] s2=new char[2*s.length()+1];
         for(int i=0;i<s.length();i++){
-            int[] temp1=maxOdd(s,i);
-            int[] temp2=maxEven(s,i);
-            if(temp1[0]>maxLength[0]) maxLength=temp1;
-            if(temp2[0]>maxLength[0]) maxLength=temp2;
+            s2[2*i]='|';
+            s2[2*i+1]=s.charAt(i);
         }
-        return s.substring(maxLength[1],maxLength[2]+1);
+        s2[2*s.length()]='|';
+        return s2;
     }
+    
+    private String removeBoundary(char[] s){
+        // result will have at least length 1
+        char[] s2=new char[(s.length-1)/2];
+        for(int i=0;i<s2.length;i++)
+            s2[i]=s[2*i+1];
+        return String.valueOf(s2);
+    }
+    
+	public String longestPalindrome(String s) {
+	    if(s.length()==0) return "";
+	    char[] s2= addBoundary(s);
+	    int[] p=new int[s2.length];
+	    int c=0, r=0;
+	    int m=0, n=0;
+	    for(int i=1; i<s2.length;i++){
+	        if(i>r){
+	            p[i]=0; m=i-1; n=i+1;
+	        }
+	        else{
+	            int i2=2*c-i;
+	            if(p[i2]<r-i) {
+	                p[i]=p[i2];
+	                m=-1;
+	            }
+	            else{
+	                p[i]=r-i;
+	                m=2*i-r-1;
+	                n=r+1;
+	            }
+	        }
+	        // update p[i] if needed
+	        while(m>=0&&n<s2.length&&s2[m]==s2[n]){
+	            m--; n++; p[i]+=1;
+	        }
+	        if(p[i]+i>r){
+	            c=i; r=p[i]+i;
+	        }
+	    }
+	    int len=0;
+	    for(int i=0;i<s2.length;i++){
+	        if(p[i]>len){
+	            len=p[i];
+	            c=i;
+	        }
+	    }
+	    return removeBoundary(Arrays.copyOfRange(s2,c-len,c+len+1));
+	}
+	
 }
