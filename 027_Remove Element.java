134273e19d51a76c79a5525158d748c234183da9
Samsong <ssydyc@gmail.com>
2015-10-20 23:16:31 -0700

    Create 027_Remove Element.java

---
+++ 027_Remove Element.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int j=nums.length-1;
+        int i=0;
+        while(i<=j){
+            if(nums[i]==val){
+                nums[i]=nums[j--];
+            }
+            else i++;
+        }
+        return j+1;
+    }
+}
