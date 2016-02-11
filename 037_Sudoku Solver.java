29ecf8d3af598cf39db3eb308936709aa30c03bd
Samsong <ssydyc@gmail.com>
2016-02-11 00:48:14 -0800

    Update 037_Sudoku Solver.java

--- 037_Sudoku Solver.java
+++ 037_Sudoku Solver.java
@@ -1,46 +1,44 @@
 public class Solution {
-    public void solveSudoku(char[][] board) {
-        int[] res=getNext(0,-1,board);
-        helper(res[0],res[1], board);
+    private void findNext(int[] cur, char[][] board) {
+        do {
+            cur[1]++;
+            if(cur[1] > 8) {
+                cur[1] = 0;
+                cur[0] ++ ;
+            }
+        } while (cur[0] < 9 && board[cur[0]][cur[1]] != '.');
         return;
     }
-    
-    private boolean helper(int x, int y, char[][] board){
-        if(x==9) return true;
-        boolean[] flag=new boolean[9];
-        Arrays.fill(flag,false);
-        for(int i=0;i<9;i++){
-            if(board[i][y]!='.')
-                flag[(int)board[i][y]-(int)'1']=true;
-            if(board[x][i]!='.')
-                flag[(int)board[x][i]-(int)'1']=true;
-        }
-        int tempx=x/3*3;
-        int tempy=y/3*3;
-        for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++){
-                if(board[tempx+i][tempy+j]!='.')
-                    flag[(int)board[tempx+i][tempy+j]-(int)'1']=true;
-            }
-        int[] res=getNext(x,y,board);
-        for(int i=0;i<9;i++){
-            if(!flag[i]){
-                board[x][y]=(char)((int)'1'+i);
-                if(helper(res[0],res[1],board)) return true;
-                board[x][y]='.';
+   
+    private boolean helper(char[][] board, int[] cur) {
+        int[] temp = {cur[0], cur[1]};
+        findNext(cur, board);
+        if(cur[0] == 9) return true; // we have reached the end
+        boolean[] possible = new boolean[9];
+        Arrays.fill(possible, true);
+        for(int i = 0; i < 9; i++)
+            if(board[i][cur[1]] != '.') possible[board[i][cur[1]] - '1'] = false;
+        for(int i = 0; i < 9; i++)
+            if(board[cur[0]][i] != '.') possible[board[cur[0]][i] - '1'] = false;
+        int tempi = cur[0] / 3 * 3, tempj = cur[1] / 3 * 3;
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++) 
+                if(board[tempi + i][tempj + j] != '.') possible[board[tempi + i][tempj + j] - '1'] = false;
+        for(int i = 0; i < 9; i++) {
+            if(possible[i]) {
+                board[cur[0]][cur[1]] = (char) ((int) '1' + i);
+                if(helper(board, cur)) return true;
             }
         }
+        board[cur[0]][cur[1]] = '.';
+        cur[0] = temp[0];
+        cur[1] = temp[1];
         return false;
     }
     
-    private int[] getNext(int x, int y, char[][] board){
-        do {
-            if(++y==9){
-                x++;
-                y=0;
-            }
-        } while(x!=9 && board[x][y]!='.');
-        int[] res={x,y};
-        return res;
+    public void solveSudoku(char[][] board) {
+        int[] cur = {0, -1};
+        helper(board, cur);
+        return;
     }
 }
